e4f13651530136520e98bae8f56dcba84c5626fc
giginet <giginet.net@gmail.com>
2011-11-01 15:48:15 +0900

    課題１おわた

--- no1.R
+++ no1.R
@@ -1,9 +1,14 @@
 X11()
-d<-matrix(c(0, 3, 5, 5, 5, 3, 0, 2, 4, 5, 5, 5, 2, 0, 1, 5, 4, 5, 1, 0), 4, 4)
+characters<-c('Ami Hutami', 'Makoto Kikuchi', 'Iori Minase', 'Miki Hosii', 'Yukiho Ogiwara', 'Azusa Miura', 'Chihaya Kisaragi')
+d<-matrix(c(0, 3, 5.5, 5, 3, 6, 8,
+            3, 0, 2, 4.5, 5.6, 0.3, 3.1,
+            5.5, 2, 0, 1, 3.3, 5,6, 4,
+            5, 4.5, 1, 0, 4, 9, 5)
+, 7, 7)
 loc<-cmdscale(d)
 loc
 x<-loc[,1]
 y<-loc[,2]
 plot(x, y, type="n")
-text(x, y, 1:4)
-repeat{}
+text(x, y, characters)
+Sys.sleep(10)
